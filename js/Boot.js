0af1767f8e7e2677265a039234411d6fb0b517fb
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-04-06 17:32:05 -0700

    added Preloader.js and preloading screen

--- js/Boot.js
+++ js/Boot.js
@@ -1,19 +1,28 @@
-BunnyDefender.Boot.prototype = {
+var CatDefender = {};
+
+CatDefender.Boot = function(game) {};
+
+CatDefender.Boot.prototype = {
 	
-	preload: function() {},
+	preload: function() {
+		this.load.image('preloaderBar', 'images/loader_bar.png');
+		this.load.image('titleimage', 'images/TitleImage.png');
+	},
 
 	create: function() {
 		this.input.maxPointers = 1;
-		this.stage.disableVisibilityChange = false; // pause game on tab change
+		this.stage.disableVisibilityChange = false;
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.minWidth = 270;
 		this.scale.minHeight = 480;
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
-		this.stage.forcePortrait = true;  // force portrait mode
-		this.scale.setScreenSize(true);  // true will force screen resize no matter what
+		this.stage.forcePortrait = true;
+		this.scale.setScreenSize(true);
 
 		this.input.addPointer();
 		this.stage.backgroundColor = '#171642';
+
+		this.state.start('Preloader');
 	}
 };
